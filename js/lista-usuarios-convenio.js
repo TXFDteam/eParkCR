34705c763ba297538b8f21f314c654af3640c198
Daniel <dzunigar@ucenfotec.ac.cr>
2020-07-25 20:11:22 -0400

    update

--- js/lista-usuarios-convenio.js
+++ js/lista-usuarios-convenio.js
@@ -21,58 +21,15 @@ let btn_activar = document.createElement('input');
 
 btn_activar.type = "checkbox";
 
-/*PRIMER ROW PARA TH*/
-let thead = document.createElement("tr");
 
-/*CICLO PARA CREAR COLUMNAS TH*/
-for (let col = 0; col < 3; col++) {
-    //CREAR CELDA*/
-    let celda = document.createElement("th");
-    /*TITULOS DE COLUMNAS, SIEMPRE LOS MISMOS*/
-    if (col == 0) {
-        celda.innerHTML = "Id";
-    }
-    if (col == 1) {
-        celda.innerHTML = "Nombre";
-    }
-    if (col == 2) {
-        celda.innerHTML = "Estado";
-    }
 
-    thead.appendChild(celda);
-}
-tabla_usuarios.appendChild(thead);
 
-/*CICLO PARA CREAR FILAS*/
-for (let row; row < 5; row++) {
-    let row = document.createElement("tr");
 
-    /*ciclo para crear celdas*/
-    for (let celdas = 0; celdas < 3; col++) {
-        /*crea celda*/
-        let celda = document.createElement("td");
-        const mostrar_usuarios = (p_id, p_nombre) => {
-            if (celdas == 0) {
-                celda = p_id;
-            }
-            if (celdas == 0) {
-                celda = p_nombre;
-            }
-        }
-        if (celdas == 2) {
-            let rinput;
-            rinput = document.createElement('input');
-            rinput.type = "checkbox";
-        }
-        celda.appendChild(rinput);
-    }
-    row.appendChild(celda);
-}
-/*
+
 const mostrar_usuarios = (p_id, p_nombre) => {
 
     tabla_usuarios.innerHTML = '';
-    tabla_usuarios.appendChild(btn_activar);
+
 
     let filas_tabla = tabla_usuarios.insertRow();
     filas_tabla.insertCell().innerHTML = p_id;
@@ -80,6 +37,8 @@ const mostrar_usuarios = (p_id, p_nombre) => {
     filas_tabla.insertCell().innerHTML = btn_activar;
 
 
-};*/
+};
 
 mostrar_usuarios('001', 'Daniel');
+mostrar_usuarios('002', 'RO');
+mostrar_usuarios('003', 'F');
--- js/simulador_bd.js
+++ js/simulador_bd.js
@@ -3,190 +3,219 @@
 
 //Parqueos---------------
 let parqueos = {
-        cant_parqueos: 3,
-        parqueo_1: {
-            nombre: 'Parqueo la maravilla',
-            ubicacion: 'San José',
-            calificacion_promedio: 5,
-            abierto: false,
-            cant_pisos: 1,
-            pisos: {
-                piso_1: {
-                    cant_espacios: 8,
-                    espacios: {
-                        espacio_1: {
-                            id: 'A1',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_2: {
-                            id: 'A2',
-                            tipo_icono: 0,
-                            ocupado: true
-                        },
-                        espacio_3: {
-                            id: 'A3',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_4: {
-                            id: 'A4',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_5: {
-                            id: 'B1',
-                            tipo_icono: 1,
-                            ocupado: true
-                        },
-                        espacio_6: {
-                            id: 'B2',
-                            tipo_icono: 1,
-                            ocupado: false
-                        },
-                        espacio_7: {
-                            id: 'C1',
-                            tipo_icono: 2,
-                            ocupado: true
-                        },
-                        espacio_8: {
-                            id: 'C2',
-                            tipo_icono: 2,
-                            ocupado: true
-                        }
+    cant_parqueos: 3,
+    parqueo_1: {
+        nombre: 'Parqueo la maravilla',
+        ubicacion: 'San José',
+        calificacion_promedio: 5,
+        abierto: false,
+        cant_pisos: 1,
+        pisos: {
+            piso_1: {
+                cant_espacios: 8,
+                espacios: {
+                    espacio_1: {
+                        id: 'A1',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_2: {
+                        id: 'A2',
+                        tipo_icono: 0,
+                        ocupado: true
+                    },
+                    espacio_3: {
+                        id: 'A3',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_4: {
+                        id: 'A4',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_5: {
+                        id: 'B1',
+                        tipo_icono: 1,
+                        ocupado: true
+                    },
+                    espacio_6: {
+                        id: 'B2',
+                        tipo_icono: 1,
+                        ocupado: false
+                    },
+                    espacio_7: {
+                        id: 'C1',
+                        tipo_icono: 2,
+                        ocupado: true
+                    },
+                    espacio_8: {
+                        id: 'C2',
+                        tipo_icono: 2,
+                        ocupado: true
                     }
                 }
             }
-        },
+        }
+    },
 
-        parqueo_2: {
-            nombre: 'Parqueo López López',
-            ubicacion: 'San José',
-            calificacion_promedio: 4.7,
-            abierto: true,
-            cant_pisos: 2,
-            pisos: {
-                piso_1: {
-                    cant_espacios: 7,
-                    espacios: {
-                        espacio_1: {
-                            id: 'A1',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_2: {
-                            id: 'A2',
-                            tipo_icono: 0,
-                            ocupado: true
-                        },
-                        espacio_3: {
-                            id: 'A3',
-                            tipo_icono: 2,
-                            ocupado: false
-                        },
-                        espacio_4: {
-                            id: 'C1',
-                            tipo_icono: 1,
-                            ocupado: false
-                        },
-                        espacio_5: {
-                            id: 'C2',
-                            tipo_icono: 1,
-                            ocupado: true
-                        },
-                        espacio_6: {
-                            id: 'B1',
-                            tipo_icono: 2,
-                            ocupado: false
-                        },
-                        espacio_7: {
-                            id: 'B2',
-                            tipo_icono: 2,
-                            ocupado: true
-                        }
+    parqueo_2: {
+        nombre: 'Parqueo López López',
+        ubicacion: 'San José',
+        calificacion_promedio: 4.7,
+        abierto: true,
+        cant_pisos: 2,
+        pisos: {
+            piso_1: {
+                cant_espacios: 7,
+                espacios: {
+                    espacio_1: {
+                        id: 'A1',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_2: {
+                        id: 'A2',
+                        tipo_icono: 0,
+                        ocupado: true
+                    },
+                    espacio_3: {
+                        id: 'A3',
+                        tipo_icono: 2,
+                        ocupado: false
+                    },
+                    espacio_4: {
+                        id: 'C1',
+                        tipo_icono: 1,
+                        ocupado: false
+                    },
+                    espacio_5: {
+                        id: 'C2',
+                        tipo_icono: 1,
+                        ocupado: true
+                    },
+                    espacio_6: {
+                        id: 'B1',
+                        tipo_icono: 2,
+                        ocupado: false
+                    },
+                    espacio_7: {
+                        id: 'B2',
+                        tipo_icono: 2,
+                        ocupado: true
                     }
-                },
-                piso_2: {
-                    cant_espacios: 3,
-                    espacios: {
-                        espacio_1: {
-                            id: 'A4',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_2: {
-                            id: 'A5',
-                            tipo_icono: 1,
-                            ocupado: false
-                        },
-                        espacio_3: {
-                            id: 'A6',
-                            tipo_icono: 2,
-                            ocupado: true
-                        }
+                }
+            },
+            piso_2: {
+                cant_espacios: 3,
+                espacios: {
+                    espacio_1: {
+                        id: 'A4',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_2: {
+                        id: 'A5',
+                        tipo_icono: 1,
+                        ocupado: false
+                    },
+                    espacio_3: {
+                        id: 'A6',
+                        tipo_icono: 2,
+                        ocupado: true
                     }
                 }
             }
-        },
+        }
+    },
 
-        parqueo_3: {
-            nombre: 'Parqueo el Covid',
-            ubicacion: 'San José',
-            calificacion_promedio: 3.85,
-            abierto: true,
-            cant_pisos: 1,
-            pisos: {
-                piso_1: {
-                    cant_espacios: 9,
-                    espacios: {
-                        espacio_1: {
-                            id: 'A1',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_2: {
-                            id: 'A2',
-                            tipo_icono: 0,
-                            ocupado: true
-                        },
-                        espacio_3: {
-                            id: 'A3',
-                            tipo_icono: 0,
-                            ocupado: false
-                        },
-                        espacio_4: {
-                            id: 'C1',
-                            tipo_icono: 1,
-                            ocupado: false
-                        },
-                        espacio_5: {
-                            id: 'C2',
-                            tipo_icono: 1,
-                            ocupado: true
-                        },
-                        espacio_6: {
-                            id: 'B1',
-                            tipo_icono: 2,
-                            ocupado: false
-                        },
-                        espacio_7: {
-                            id: 'B2',
-                            tipo_icono: 2,
-                            ocupado: true
-                        },
-                        espacio_8: {
-                            id: 'B3',
-                            tipo_icono: 2,
-                            ocupado: true
-                        },
-                        espacio_9: {
-                            id: 'B4',
-                            tipo_icono: 2,
-                            ocupado: true
-                        }
+    parqueo_3: {
+        nombre: 'Parqueo el Covid',
+        ubicacion: 'San José',
+        calificacion_promedio: 3.85,
+        abierto: true,
+        cant_pisos: 1,
+        pisos: {
+            piso_1: {
+                cant_espacios: 9,
+                espacios: {
+                    espacio_1: {
+                        id: 'A1',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_2: {
+                        id: 'A2',
+                        tipo_icono: 0,
+                        ocupado: true
+                    },
+                    espacio_3: {
+                        id: 'A3',
+                        tipo_icono: 0,
+                        ocupado: false
+                    },
+                    espacio_4: {
+                        id: 'C1',
+                        tipo_icono: 1,
+                        ocupado: false
+                    },
+                    espacio_5: {
+                        id: 'C2',
+                        tipo_icono: 1,
+                        ocupado: true
+                    },
+                    espacio_6: {
+                        id: 'B1',
+                        tipo_icono: 2,
+                        ocupado: false
+                    },
+                    espacio_7: {
+                        id: 'B2',
+                        tipo_icono: 2,
+                        ocupado: true
+                    },
+                    espacio_8: {
+                        id: 'B3',
+                        tipo_icono: 2,
+                        ocupado: true
+                    },
+                    espacio_9: {
+                        id: 'B4',
+                        tipo_icono: 2,
+                        ocupado: true
                     }
                 }
             }
+        }
+    },
+};
+//Final parqueos---------------
+
+
+//USUARIOS------------------
+let usuarios = {
+
+};
+
+let empresas = {
+    lista_empresas: {
+        empresa_1: {
+            correo_empresa: 'x1@gmail.com',
+            cedula_empresa: '111',
+            nombre_empresa: 'Adidas',
+            convenio: '11'
         },
+        empresa_2: {
+            correo_empresa: 'x2@gmail.com',
+            cedula_empresa: '112',
+            nombre_empresa: 'Banco Nacional',
+            convenio: '12'
+        },
+        empresa_3: {
+            correo_empresa: 'x1@gmail.com',
+            cedula_empresa: '113',
+            nombre_empresa: 'Banco popular',
+            convenio: '13'
+        }
     }
-    //Final parqueos---------------
+};
