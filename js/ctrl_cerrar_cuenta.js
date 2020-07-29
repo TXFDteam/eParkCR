f8417f9b83068a1ef83376728e5a79be8c24188d
Xavier0725 <xlopezc@ucenfotec.ac.cr>
2020-07-29 07:44:40 -0600

    push
    push

---
+++ js/ctrl_cerrar_cuenta.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const btn_cerrar_cuenta = document.querySelector('#btn-cerrar-cuenta');
+
+//Se importa el botón en una variable para poder crear el evento y posteriormente proceder con el cierre de la cuenta
+
+
+
+const cerrar_cuenta = () => {
+
+    // función para cerrar cuenta
+
+    Swal.fire({
+        title: 'Cerrar cuenta',
+        text: "¿Está seguro que desea cerrar la cuenta?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'No',
+        confirmButtonText: 'Sí'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'Cerar cuenta',
+                'La cuenta ha sido cerrada exitosamente.',
+                'success'
+            )
+        }
+    })
+
+};
+
+btn_cerrar_cuenta.addEventListener('click', cerrar_cuenta);
