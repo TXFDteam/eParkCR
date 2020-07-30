eb0a08cbc52c905f91c8511d1d0358b1e0673435
Daniel <dzunigar@ucenfotec.ac.cr>
2020-07-30 19:47:03 -0400

    control sesiones e iniciar sesion

---
+++ js/ctrl-iniciar-sesion.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+let input_correo = document.querySelector('#correo');
+let input_contrasenna = document.querySelector('#contrasenna');
+let btn_iniciar_sesion = document.querySelector('#btn-iniciar-sesion');
+
+
+let obtener_datos_y_validar = () => {
+    let correo = input_correo.value;
+    let contrasenna = input_contrasenna.value;
+
+    if ()
+};
+
+//let validar_datos = () => {};
+
+btn_iniciar_sesion.addEventListener('click', () => {
+    obtener_datos();
+});
