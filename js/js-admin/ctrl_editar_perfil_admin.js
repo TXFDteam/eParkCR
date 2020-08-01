ebb2aa85c06d60a1adb6412e313fcf7d94fce28a
Xavier0725 <xlopezc@ucenfotec.ac.cr>
2020-08-01 13:55:03 -0600

    push
    push

--- js/js-admin/ctrl_editar_perfil_admin.js
+++ js/js-admin/ctrl_editar_perfil_admin.js
@@ -1,12 +1,33 @@
 'use strict';
 
-const btn_editar_perfil_admin = document.querySelector('#btn-editar-perfil-admin');
 
-const redireccionar_perfil_admin = () => {
-    window.location.assign('../../html/htmls-admin/editar_perfil_administrador.html');
+
+
+const input_nombre_admin = document.querySelector('#nuevo-nombre-admin');
+const input_telefono_admin = document.querySelector('#nuevo-telefono-admin');
+const input_comision_admin = document.querySelector('#nueva-comision-admin');
+
+
+const btn_guardar_cambios_admin = document.querySelector("#btn-guardar-cambios-admin");
+
+
+
+
+const guardar_cambios_admin = () => {
+
+    administrador.nombre = input_nombre_admin.value;
+    administrador.comision = input_comision_admin.value;
+    dministrador.telefono = input_telefono_admin.value;
+
+
+
+    Swal.fire(
+        'Perfil actualizado',
+        'Por favor espera unos minutos para que los cambios se reflejen en el sistema',
+        'success'
+    )
 };
 
-btn_editar_perfil_admin.addEventListener('click', redireccionar_perfil_admin);
 
 
-const input_nombre = document.querySelector('#nuevo-nombre-admin');
+btn_guardar_cambios_admin.addEventListener('click', guardar_cambios_admin);
--- js/js-empresa/ctrl_editar_perfil_empresa.js
+++ js/js-empresa/ctrl_editar_perfil_empresa.js
@@ -7,3 +7,23 @@ const redireccionar_perfil_empresa = () => {
 };
 
 btn_editar_perfil_empresa.addEventListener('click', redireccionar_perfil_empresa);
+
+
+const nuevo_nombre_empresa = document.querySelector('#nuevo-nombre-empresa');
+const boton_guardar_datos_empresa = document.querySelector('#btn-guardar-cambios-empresa');
+
+
+const guardar_datos_empresa = (identificador_empresa) => {
+
+    empresas.lista_empresas[identificador_empresa].nombre_empresa = nuevo_nombre_empresa.value;
+
+
+    Swal.fire(
+        'Perfil actualizado',
+        'Por favor espera unos minutos para que los cambios se reflejen en el sistema',
+        'success'
+    )
+};
+
+
+guardar_datos_empresa.addEventListener('click', guardar_datos_empresa(identificador_empresa));
