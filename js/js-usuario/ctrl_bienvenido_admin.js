7713ab2811d5ff19981f8686238e2633814f554b
francisvs56 <fvanschies@ucenfotec.ac.cr>
2020-08-01 16:24:32 -0600

    ajustes bienvenidos usuarios

---
+++ js/js-usuario/ctrl_bienvenido_admin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const btn_solicitudesRegistro = document.querySelector('#btn_solicitudesRegistro');
+
+const redireccionar_bienvenido_admin = () => {
+    window.location.assign('../../html/htmls-usuarios/lista-solicitudes-registro.html');
+};
+
+btn_solicitudesRegistro.addEventListener('click', redireccionar_bienvenido_admin);
--- js/js-usuario/ctrl_bienvenido_cliente.js
+++ js/js-usuario/ctrl_bienvenido_cliente.js
@@ -2,8 +2,39 @@
 
 const btn_buscarParqueos = document.querySelector('#btn_buscarParqueos');
 
+const txt_titulo = document.querySelector('#txt-titulo-bienvenida');
+
+let usuario_ingresado;
+
+const actualizar_datos = () => {
+    txt_titulo.textContent = 'Bienvenido ' + usuario_ingresado.nombre_usuario;
+    btn_principal.textContent = 'Buscar parqueos';
+};
+
+
+const obtener_usuario_ingresado = () => {
+    let contrasenna = localStorage.getItem('contrasenna');
+    let correo = localStorage.getItem('correo');
+
+    for (let i = 1; i < usuarios.cant_usuarios; i++) {
+        let identificador_usuario = ('usuario' + i);
+        let usuario_actual = usuarios[identificador_usuario];
+
+        if (correo == usuario_actual.correo_usuario && contrasenna == usuario_actual.contraseña) {
+            return usuario_actual;
+        }
+
+    }
+    usuario_ingresado = '';
+};
+
+
+
 const redireccionar_bienvenido_cliente = () => {
     window.location.assign('../../html/htmls-usuarios/buscar_parqueos.html');
 };
+
+usuario_ingresado = obtener_usuario_ingresado();
+actualizar_datos();
 
 btn_buscarParqueos.addEventListener('click', redireccionar_bienvenido_cliente);
