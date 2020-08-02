d18d733cc1128fbc3f41ebb34cdca58fde06bb7b
Daniel <dzunigar@ucenfotec.ac.cr>
2020-08-02 17:37:15 -0400

    cambios redirecciones en iniciar sesión

--- js/js-admin/ctrl_bienvenido_admin.js
+++ js/js-admin/ctrl_bienvenido_admin.js
@@ -2,8 +2,6 @@
 
 const btn_solicitudesRegistro = document.querySelector('#btn_solicitudesRegistro');
 
-const redireccionar_bienvenido_admin = () => {
+btn_solicitudesRegistro.addEventListener('click', function() {
     window.location.assign('../../html/htmls-admin/lista-solicitudes-registro.html');
-};
-
-btn_solicitudesRegistro.addEventListener('click', redireccionar_bienvenido_admin);
+});
--- js/js-parqueo/ctrl_bienvenido_prq.js
+++ js/js-parqueo/ctrl_bienvenido_prq.js
@@ -2,8 +2,7 @@
 
 const btn_mis_parqueos = document.querySelector('#btn_mis_parqueos');
 
-const redireccionar_bienvenido_parqueo = () => {
-    window.location.assign('../../html/htmls-parqueos/prq_mis_parqueos.html');
-};
 
-btn_mis_parqueos.addEventListener('click', redireccionar_bienvenido_parqueo);
+btn_mis_parqueos.addEventListener('click', function() {
+    window.location.assign('../../html/htmls-parqueos/prq_mis_parqueos.html');
+});
