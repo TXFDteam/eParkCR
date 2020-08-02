72ca76703abfde6bd87391ebd6f240730213b583
Daniel <dzunigar@ucenfotec.ac.cr>
2020-08-02 18:59:50 -0400

    cambios grandes en reporte ingresos admin

--- js/js-admin/ctrl_reporte_ingresos_admin.js
+++ js/js-admin/ctrl_reporte_ingresos_admin.js
@@ -3,35 +3,15 @@
 //Relacionar la informacion del HTML
 //const tabla_reporte_ingresos_header = document.querySelector('#tabla_reporte_ingresos thead');
 const tabla_reporte_ingresos_body = document.querySelector('#tabla_reporte_ingresos tbody');
+const tabla_reporte_ingresos_header = document.querySelector('#tabla_reporte_ingresos thead');
 const btn_descargar = document.querySelector('#btn-descargar');
 
 
 let array_reservas;
-
-/* --------------------- Identificador de usuario actual -------------------- */
-//Para probar el JS se preestablece usuario1: const id_usuario = usuarios.usuario1;
-
-/*
-const obtener_usuario_ingresado = () => {
-    let contrasenna = localStorage.getItem('contrasenna');
-    let correo = localStorage.getItem('correo');
-
-    for (let i = 1; i < usuarios.cant_usuarios; i++) {
-        let identificador_usuario = ('usuario' + i);
-        let usuario_actual = usuarios[identificador_usuario];
-
-        if (correo == usuario_actual.correo_usuario && contrasenna == usuario_actual.contraseña) {
-            usuario_actual = usuarios[identificador_usuario].id_usuario;
-            break;
-        }
-    }
-};
-
-obtener_usuario_ingresado();*/
+let head;
+let fila;
 
 
-
-//const id_usuario = obtener_usuario_ingresado();
 
 btn_descargar.addEventListener('click', function() {
     let printContent = document.querySelector('#tabla_reporte_ingresos');
@@ -42,53 +22,41 @@ btn_descargar.addEventListener('click', function() {
 
 
 
+const listar_datos_tabla = (parq) => {
+    let fila = tabla_reporte_ingresos_body.insertRow();
 
-const listar_reporte = () => {
+    fila.insertCell().innerHTML = parq.codigo;
+    fila.insertCell().innerHTML = parq.nombre;
+    fila.insertCell().innerHTML = parq.hora_entrada;
+    /*
+        if (reservas[identificador_reserva].estado_reserva == 'Paga') {
+            fila.insertCell().innerHTML = reservas[identificador_reserva].monto_final;
+            fila.insertCell().innerHTML = 'Cancelado' + boton_recibo;
 
-
-    for (let i = 1; i <= parqueos.cant_parqueos; i++) {
-        let identificador_parqueo = ('parqueo_' + i);
-        let nombre_parqueo = parqueos[identificador_parqueo].nombre;
-
-        for (let j = 1; i <= reservas.cant_reservas; j++) {
-            let identificador_reserva = ('reserva' + j);
-
-            console.log('check 1 ' + reservas.reserva1.parqueo_seleccionado);
-            console.log('check 2 ' + identificador_reserva);
-            console.log('check 3 ' + reservas[identificador_reserva]);
-
-            if (reservas[identificador_reserva].parqueo_seleccionado == nombre_parqueo) {
-
-
-            }
-
-        };
-    };
+        } else {
+            fila.insertCell().innerHTML = reservas[identificador_reserva].monto_total;
+            fila.insertCell().innerHTML = reservas[identificador_reserva].estado_reserva;
+        }
+    */
+    tabla_reporte_ingresos_body.appendChild(fila);
 };
 
+const listar_reporte = () => {
+    tabla_reporte_ingresos_header.innerHTML = '';
+    tabla_reporte_ingresos_body.innerHTML = '';
+    head = tabla_reporte_ingresos_header.insertRow();
+    head.insertCell().innerHTML = 'Id';
+    head.insertCell().innerHTML = 'Nombre del parqueo';
+    head.insertCell().innerHTML = 'Ingresos totales';
+    head.insertCell().innerHTML = 'Total comisión';
+    head.insertCell().innerHTML = 'Ganancias del parqueo';
+
+    tabla_reporte_ingresos_header.appendChild(head);
+    //AQUÍ CREA LOS DATOS DE LA TABLA
+    for (let i = 1; i <= parqueos.cant_parqueos; i++) {
+        let identificador_parqueo = ('parqueo_' + i);
+        listar_datos_tabla(parqueos[identificador_parqueo]);
+    }
+};
 
 listar_reporte();
-
-
-
-/* let fila = tabla_hist_reserv_body.insertRow();
-
-
-fila.insertCell().innerHTML = reservas[identificador_reserva].fecha_reserva;
-fila.insertCell().innerHTML = reservas[identificador_reserva].parqueo_seleccionado;
-fila.insertCell().innerHTML = reservas[identificador_reserva].hora_entrada;
-fila.insertCell().innerHTML = reservas[identificador_reserva].hora_salida;
-
-
-if (reservas[identificador_reserva].estado_reserva == 'Paga') {
-fila.insertCell().innerHTML = reservas[identificador_reserva].monto_final;
-fila.insertCell().innerHTML = 'Cancelado' + boton_recibo;
-
-} else {
-fila.insertCell().innerHTML = reservas[identificador_reserva].monto_total;
-fila.insertCell().innerHTML = reservas[identificador_reserva].estado_reserva;
-}
-
-
-
-tabla_hist_reserv_body.appendChild(fila);*/
