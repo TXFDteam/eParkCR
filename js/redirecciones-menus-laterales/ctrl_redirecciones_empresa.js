dc58bbdc63591ffc0f4b4f495f2aaa28781cf886
Daniel <dzunigar@ucenfotec.ac.cr>
2020-08-02 19:55:16 -0400

    cambios estilos

--- js/redirecciones-menus-laterales/ctrl_redirecciones_empresa.js
+++ js/redirecciones-menus-laterales/ctrl_redirecciones_empresa.js
@@ -4,11 +4,10 @@ const inicio = document.querySelector('#primer-link');
 const convenios = document.querySelector('#link-convenios-empresa');
 const config_cuenta_empresa = document.querySelector('#link-config-cuenta');
 
-/*FALTA CREAR HTML DE BIENVENIDA
+
 inicio.addEventListener('click', function() {
-    solicitudes_registro.href = '../../html/htmls-admin/lista-solicitudes-registro.html';
-    //marcar_link(solicitudes_registro);
-});*/
+    inicio.href = '../../html/htmls-empresas/bienvenido_empresa.html';
+});
 
 convenios.addEventListener('click', function() {
     convenios.href = '../../html/htmls-empresas/convenios-empresa.html';
