b6a804f77db55e9db868eec129d5b955368816d2
Xavier0725 <xlopezc@ucenfotec.ac.cr>
2020-08-02 18:24:05 -0600

    push
    push

--- js/js-usuario/ctrl_perfil_cliente.js
+++ js/js-usuario/ctrl_perfil_cliente.js
@@ -22,8 +22,9 @@ btn_editar_perfil_cliente.addEventListener('click', editar_perfil_cliente);
 
 let nombre_cliente = document.querySelector('#nombre-cliente');
 let correo_electronico = document.querySelector('#correo-cliente');
-let tarjeta_cliente = document.querySelector('#tarjeta-cliente');
+let numero_identificacion_cliente = document.querySelector('#numero-identificacion-cliente');
 let nacimiento_cliente = document.querySelector('#nacimiento-cliente');
+let tarjeta_cliente = document.querySelector('#tarjeta-cliente');
 
 let correo_usuario = localStorage.getItem('correo');
 console.log(correo_usuario);
@@ -42,6 +43,7 @@ for (let u = 1; u <= usuarios.cant_usuarios; u++) {
         nombre_cliente.innerHTML = usuarios[identificador_usuario].nombre_usuario;
         correo_electronico.innerHTML = usuarios[identificador_usuario].correo_usuario;
         nacimiento_cliente.innerHTML = usuarios[identificador_usuario].fecha_nacimiento;
+        numero_identificacion_cliente.innerHTML = usuarios[identificador_usuario].n_identificacion;
         //ESTE FOR VERIFICA LA TARJETA QUE TIENE COMO PREDETERMINADA
         for (let t = 1; t <= 10; t++) {
             let identificador_tarjeta = ('tarjeta_' + t);
