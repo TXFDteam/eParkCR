45a382904e01559b097355e8253b2d147a2848b9
francisvs56 <fvanschies@ucenfotec.ac.cr>
2020-08-03 00:26:51 -0600

    formularios y validaciones

--- js/CI_registro_usuarios_validar.js
+++ js/CI_registro_usuarios_validar.js
@@ -16,6 +16,22 @@ btn_yaTenesCuenta.addEventListener('click', function() {
     window.location.assign('iniciar-sesion.html')
 });
 
+const calcular_edad = (pfecha_nacimiento) => {
+    let fecha_actual = new Date();
+    let edad = 0;
+
+    edad = fecha_actual.getFullYear() - pfecha_nacimiento.getFullYear();
+
+    if (fecha_actual.getMonth() < pfecha_nacimiento.getMonth()) {
+        edad -= 1;
+    } else {
+        if (fecha_actual.getMonth() == pfecha_nacimiento.getMonth() && fecha_actual.getDate() < pfecha_nacimiento.getDate()) {
+            edad -= 1;
+        }
+
+    }
+    return edad;
+};
 
 const validar = () => {
     let error;
@@ -43,58 +59,22 @@ const validar = () => {
         input_correo.classList.add('error');
     }
 
-    var mailformat = /^\S+@\S+$/;
-    if (input_correo.value.match(mailformat)) {
-        input_correo.classList.remove('error');
+
+    var nacimiento = new Date(input_fechaNacimiento.value);
+    var edad = calcular_edad(nacimiento);
+
+    if (edad >= 18) {
+        input_fechaNacimiento.classList.remove('error');
     } else {
         error = true;
-        input_correo.classList.add('error');
+        input_fechaNacimiento.classList.add('error');
     }
-
-
-
-
-    // //Get the date from the TextBox.
-    // var dateString = document.getElementById("txtDate").value;
-    // var regex = /(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$/;
-
-    // //Check whether valid dd/MM/yyyy Date Format.
-    // if (regex.test(dateString)) {
-    //     var parts = dateString.split("/");
-    //     var dtDOB = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
-    //     var dtCurrent = new Date();
-    //     lblError.innerHTML = "Eligibility 18 years ONLY."
-    //     if (dtCurrent.getFullYear() - dtDOB.getFullYear() < 18) {
-    //         return false;
-    //     }
-
-    //     if (dtCurrent.getFullYear() - dtDOB.getFullYear() == 18) {
-
-    //         //CD: 11/06/2018 and DB: 15/07/2000. Will turned 18 on 15/07/2018.
-    //         if (dtCurrent.getMonth() < dtDOB.getMonth()) {
-    //             return false;
-    //         }
-    //         if (dtCurrent.getMonth() == dtDOB.getMonth()) {
-    //             //CD: 11/06/2018 and DB: 15/06/2000. Will turned 18 on 15/06/2018.
-    //             if (dtCurrent.getDate() < dtDOB.getDate()) {
-    //                 return false;
-    //             }
-    //         }
-    //     }
-    //     lblError.innerHTML = "";
-    //     return true;
-    // } else {
-    //     lblError.innerHTML = "Enter date in dd/MM/yyyy format ONLY."
-    //     return false;
-    // }
-
-
-
-
 
 
     return error;
 };
+
+
 
 
 
