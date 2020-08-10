df58a0d55422f848db9a6ca5458fc4ca17539fea
TaylorSeguraVindas <tsegurav@ucenfotec.ac.cr>
2020-08-09 20:36:45 -0600

    Ahora se limpia el localStorage al cerrar sesión

--- public/js/control-sesiones.js
+++ public/js/control-sesiones.js
@@ -5,5 +5,19 @@ let a_cerrar_sesion = document.querySelector('#link-cerrar-sesion');
 if (a_cerrar_sesion) {
     a_cerrar_sesion.addEventListener('click', function() {
         a_cerrar_sesion.href = "../../index.html";
+
+        //Limpiar el local storage.
+        localStorage.setItem('correo', '');
+        localStorage.setItem('contrasenna', '');
     })
 };
+
+const verificar_usuario_ingresado = () => {
+    //Si no hay ningún usuario ingresado se vuelve al landing page.
+    if (localStorage.getItem('correo') == '' || localStorage.getItem('contrasenna') == '') {
+        window.location.assign('../../index.html');
+    }
+};
+
+verificar_usuario_ingresado();
+console.log('Si es este.');
