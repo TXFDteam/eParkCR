090933e78c1b162b88c57c56459d661d14decfeb
Daniel <dzunigar@ucenfotec.ac.cr>
2020-08-10 11:44:30 -0400

    Revert "varios"
    This reverts commit c8e9fbed3e8c0469d3a4dc2ac945ebeeab925d68.

--- js/js-parqueo/ctrl_bienvenido_prq.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const btn_mis_parqueos = document.querySelector('#btn_mis_parqueos');
-
-
-btn_mis_parqueos.addEventListener('click', function() {
-    window.location.assign('../../html/htmls-parqueos/prq_mis_parqueos.html');
-});
