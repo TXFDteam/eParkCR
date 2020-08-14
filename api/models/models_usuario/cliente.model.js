65efad3c1c50d7376633a7b63364cd93f19eb4ce
Daniel <dzunigar@ucenfotec.ac.cr>
2020-08-13 21:40:49 -0400

    model cliente

--- api/models/models_usuario/cliente.model.js
+++ api/models/models_usuario/cliente.model.js
@@ -9,14 +9,14 @@ const schema_cliente = new mongoose.Schema({
     n_identificacion: { type: String, required: true, unique: true },
     fecha_nacimiento: { type: String, required: true, unique: false },
     contraseña: { type: String, required: true, unique: false },
-    //foto_perfil: { type: String, required: false, unique: false },
-    /*tarjetas: [{
+    foto_perfil: { type: String, required: false, unique: false },
+    tarjetas: [{
         numero_tarjeta: { type: String, required: false, unique: true },
         fecha_expiracion: { type: String, required: false, unique: false },
         predeterminada: { type: Boolean, required: false, unique: false },
         nombre_tarjeta: { type: String, required: false, unique: false },
         codigo_seguridad: { type: Number, required: false, unique: false }
-    }],*/
+    }],
     estado_general: { type: String, required: true, unique: false },
     otp: { type: Number, required: false, unique: false }
 
