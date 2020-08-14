e826296a9c989d7193c587297714a52b49274bfb
Daniel <dzunigar@ucenfotec.ac.cr>
2020-08-13 22:09:24 -0400

    cambio registro usuarios

--- api/models/models_usuario/cliente.model.js
+++ api/models/models_usuario/cliente.model.js
@@ -10,13 +10,6 @@ const schema_cliente = new mongoose.Schema({
     fecha_nacimiento: { type: String, required: true, unique: false },
     contraseña: { type: String, required: true, unique: false },
     foto_perfil: { type: String, required: false, unique: false },
-    tarjetas: [{
-        numero_tarjeta: { type: String, required: false, unique: true },
-        fecha_expiracion: { type: String, required: false, unique: false },
-        predeterminada: { type: Boolean, required: false, unique: false },
-        nombre_tarjeta: { type: String, required: false, unique: false },
-        codigo_seguridad: { type: Number, required: false, unique: false }
-    }],
     estado_general: { type: String, required: true, unique: false },
     otp: { type: Number, required: false, unique: false }
 
