4b3cb2b9b8e758e5e93fef803f0ae331b6800042
francisvs56 <fvanschies@ucenfotec.ac.cr>
2020-08-13 19:56:18 -0600

    obtener adentro de registrar servicio empresas

--- public/js/js-servicio/servicio_empresa/modificar_servicio_empresas.js
+++ public/js/js-servicio/servicio_empresa/modificar_servicio_empresas.js
@@ -20,3 +20,22 @@ const modificar_empresa = async(pid, pcorreo, pnombre, pencargado, pfoto) => {
             console.log(error);
         });
 };
+
+const obtener_empresas = async() => {
+    let empresas;
+    // GET request
+    await axios({
+            method: 'get',
+            url: 'http://localhost:3000/api/listar_empresas',
+            responseType: 'json'
+        })
+        .then((response) => {
+            empresas = response.data.lista_empresas;
+        })
+        .catch((response) => {
+            console.log(error);
+
+        });
+
+    return empresas;
+};
