1a15c06ae9b736ada22e2f5c218aaab63b794ae1
francisvs56 <fvanschies@ucenfotec.ac.cr>
2020-08-19 19:58:23 -0600

    estilos cliente foto perfil

--- api/models/models_parqueo/convenios.model.js
+++ api/models/models_parqueo/convenios.model.js
@@ -4,13 +4,9 @@
 const mongoose = require('mongoose');
 
 //Atributos para guardar en la colección.
-//Para las redes sociales se deben usar los siguientes datos:
-//Facebook: facebook
-//Twitter: twitter
-//Instagram: instagram
+
 
 const schema_convenio = new mongoose.Schema({
-    codigo_convenio: { type: String, required: true, unique: false },
     porcentaje_convenio: { type: Number, required: true, unique: false },
     fecha_vencimiento_convenio: { type: String, required: true, unique: false },
 });
